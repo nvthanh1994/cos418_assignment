abc8623cdb2d98247d7ad475dd4c37829ead9d2a
Thanh Nguyen <nvthanh1994@gmail.com>
2024-04-23 01:29:48 +0700

    complete assignment 1-1

--- assignment1-1/q2.go
+++ assignment1-1/q2.go
@@ -2,8 +2,12 @@ package cos418_hw1_1
 
 import (
 	"bufio"
+	"fmt"
 	"io"
+	"math/rand"
+	"os"
 	"strconv"
+	"time"
 )
 
 // Sum numbers from channel `nums` and output sum to `out`.
@@ -12,7 +16,17 @@ import (
 func sumWorker(nums chan int, out chan int) {
 	// TODO: implement me
 	// HINT: use for loop over `nums`
+
+	sum := 0
+	for num := range nums {
+		sum += num
+	}
+	fmt.Println(sum)
+	out <- sum
+	close(out)
 }
+
+type ch chan int
 
 // Read integers from the file `fileName` and return sum of all values.
 // This function must launch `num` go routines running
@@ -23,7 +37,44 @@ func sum(num int, fileName string) int {
 	// TODO: implement me
 	// HINT: use `readInts` and `sumWorkers`
 	// HINT: used buffered channels for splitting numbers between workers
-	return 0
+	r, err := os.Open(fileName)
+	checkError(err)
+	defer r.Close()
+
+	arr, err := readInts(r)
+	checkError(err)
+
+	channels := make([]ch, num)
+	outIntermediate := make([]ch, num)
+	BUFFER_SIZE := len(arr) + 1
+
+	for i := 0; i < num; i++ {
+		channels[i] = make(chan int, BUFFER_SIZE)
+		outIntermediate[i] = make(chan int)
+	}
+
+	// Map phase
+	for i := 0; i < len(arr); i++ {
+		random := rand1toN(num)
+		channels[random] <- arr[i] // Distribute value to n channels
+	}
+
+	for i := 0; i < num; i++ {
+		close(channels[i]) // Need to close so range in sumWorker can end or range  will block forever
+	}
+
+	for i := 0; i < num; i++ {
+		go sumWorker(channels[i], outIntermediate[i])
+	}
+
+	// Reduce phase
+	total := 0
+	for _, out := range outIntermediate {
+		total += <-out
+	}
+	fmt.Println(total)
+
+	return total
 }
 
 // Read a list of integers separated by whitespace from `r`.
@@ -42,4 +93,10 @@ func readInts(r io.Reader) ([]int, error) {
 		elems = append(elems, val)
 	}
 	return elems, nil
+}
+
+func rand1toN(maxx int) int {
+	rand.Seed(time.Now().UnixNano())
+	n := rand.Intn(maxx)
+	return n
 }
